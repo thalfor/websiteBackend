4d321b28c1ffc2314071a57f0f4b5413e59ae464
thalfor <thales.fornazari@gmail.com>
2024-10-17 12:23:07 -0300

    database updating

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 //
 const express = require("express");
 const routes = require("./routes");
+const database = require("./database");
 //
 const app = express();
 app.use(express.json());
 app.use(routes);
+database();
 //
 const PORT = 3000;
 app.listen(PORT, () => console.log(`server running at port ${PORT}`));
