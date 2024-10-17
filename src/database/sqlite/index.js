8a7c6aa345d227b44a7fdcdf57244c49df970692
thalfor <thales.fornazari@gmail.com>
2024-10-17 12:18:56 -0300

    database updating + installed sqlite and sqlite3

---
+++ src/database/sqlite/index.js
@@ -0,0 +1,10 @@
+//
+const sqlite3 = require("sqlite3");
+const sqlite = require("sqlite");
+//
+async function sqliteConnection(){
+  const database = await sqlite.open({
+    filename: "../../database";
+  })
+}
+//
