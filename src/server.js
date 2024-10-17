4c15e213da62d552a5acfd52bf9647d2de89c0cc
thalfor <thales.fornazari@gmail.com>
2024-10-17 11:35:50 -0300

    server organization + nodemon + express

---
+++ src/server.js
@@ -0,0 +1,12 @@
+//
+const express = require("express");
+//
+const app = express();
+const PORT = 3000;
+//
+app.get("/", (request, response) => {
+  
+})
+//
+app.listen(PORT, () => console.log(`server running at port ${PORT}`));
+//
