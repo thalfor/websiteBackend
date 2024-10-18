b6f156ef15ca57323b1aff5e04b9ee50ec9f1a21
thalfor <thales.fornazari@gmail.com>
2024-10-18 10:16:14 -0300

    updating for user creation along with Insomnia and Beekeper

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -7,8 +7,8 @@ class UsersController {
     const { email, password } = request.body;
 
     const database = await sqliteConnection();
+    
     const checkUserExists = await database.get("select * from users where email = (?)", [email]);
-
     if(checkUserExists){
       throw new AppError("this email already exists");
     }
