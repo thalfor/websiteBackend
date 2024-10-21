8ad590f51cd6f0d65b739d3225c76a2c5dce147a
thalfor <thales.fornazari@gmail.com>
2024-10-21 18:58:00 -0300

    added cors to comunicate between ports

--- src/server.js
+++ src/server.js
@@ -2,14 +2,16 @@
 require('express-async-errors');
 const AppError = require('./utils/AppError');
 //
-const express = require("express");
-const routes = require("./routes");
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
 //
 const migrationsRun = require('./database/sqlite/migrations');
 migrationsRun();
 //
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 //
 app.use((error, request, response, nexto) => {
